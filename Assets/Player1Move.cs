b230eb220c15555f6cf9f931cd122a6d306f2619
LiliaIsABell <lilisa_alugo@yahoo.com>
2021-02-27 18:50:23 -0500

    Added local multiplayer

---
+++ Assets/Player1Move.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code from Fadrik on Youtube
+//https://www.youtube.com/watch?v=UKqFvXaTuvQ
+
+
+public class Player1Move : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.left * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.right * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.forward * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.back * speed * Time.deltaTime);
+
+        }
+    }
+}
---
+++ Assets/Player2Move.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code from Fadrik on Youtube
+//https://www.youtube.com/watch?v=UKqFvXaTuvQ
+
+
+public class Player2Move : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D)){
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.left * speed * Time.deltaTime);
+            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.right * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.forward * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            this.gameObject.GetComponent<Transform>().Translate(Vector3.back * speed * Time.deltaTime);
+
+        }
+    }
+}

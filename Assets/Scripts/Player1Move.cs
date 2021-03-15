1965e2444d89e131223db56372abed1c4d302b8b
LiliaIsABell <lilisa_alugo@yahoo.com>
2021-03-15 15:48:38 -0400

    Attempted punch mechanic and started title screen

--- Assets/Scripts/Player1Move.cs
+++ Assets/Scripts/Player1Move.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 // Code from Fadrik on Youtube
 //https://www.youtube.com/watch?v=UKqFvXaTuvQ
@@ -12,12 +14,30 @@ public class Player1Move : MonoBehaviour
 
     public float speed = 12f;
 
+    //Punching variables
+    //
+    //public string punchtag;
+
+    public Text pointsdisplay;
+
+    int score = 0;
+
+    public GameObject fist;
+    public GameObject target;
+
+    // From Charles code
+    public bool punching = false;
+
+
+
+  
 
 
     // Update is called once per frame
     void Update()
     {
-
+        //Movement code
+        //
         if (Input.GetKey(KeyCode.DownArrow))
         {
 
@@ -45,5 +65,48 @@ public class Player1Move : MonoBehaviour
             this.gameObject.GetComponent<Transform>().Translate(Vector3.back * speed * Time.deltaTime);
 
         }
+
+        //Punching code
+        //
+        float distance = (fist.transform.position - target.transform.position).magnitude;
+
+
+        if (distance <= 0.7)
+        {
+            score += 1;
+            pointsdisplay.text = "Points: " + score;
+
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            StartCoroutine(Attack(0.15f, 3f, fist.transform.right));
+        }
     }
+
+    IEnumerator Attack(float time, float distance, Vector3 direction)
+
+    {
+        punching = true;
+        float timer = 0.0f;
+        direction.Normalize();
+        while (timer <= time)
+        {
+            fist.transform.position = target.transform.position + (Mathf.Sin(timer / time * Mathf.PI) + 1.0f) * direction;
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        fist.transform.position = target.transform.position;
+        punching = false;
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.collider.gameObject.CompareTag(punchtag))
+    //    {
+
+    //        SceneManager.LoadScene(0);
+
+    //    }
+    //}
 }
--- Assets/Scripts/Punch.cs
+++ Assets/Scripts/Punch.cs
@@ -2,34 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Punch : MonoBehaviour
 {
+
+    public string punchtag;
+
     public Text pointsdisplay;
 
     int score = 0;
 
-    public GameObject proximity1;
-    public GameObject proximity2;
+    public GameObject fist;
+    public GameObject target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // From Charles code
+    public bool punching = false;
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distance = (proximity1.transform.position - proximity2.transform.position).magnitude;
-
-        //textcomponent.text = "Distance" + distance;
+        float distance = (fist.transform.position - target.transform.position).magnitude;
 
 
         if (distance <= 0.7)
         {
-            score = score + 1;
+            score += 1;
             pointsdisplay.text = "Points: " + score;
+            
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            StartCoroutine(Attack(0.15f, 3f, fist.transform.right));
+        }
+
+
+    }
+
+
+    // From Charles code
+    IEnumerator Attack(float time, float distance, Vector3 direction)
+    {
+        punching = true;
+        float timer = 0.0f;
+        direction.Normalize();
+        while (timer <= time)
+        {
+            fist.transform.position = target.transform.position + (Mathf.Sin(timer / time * Mathf.PI) + 1.0f) * direction;
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        fist.transform.position = target.transform.position;
+        punching = false;
+    }
+
+private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag(punchtag))
+        {
+
+            SceneManager.LoadScene(0);
+
         }
     }
 }
